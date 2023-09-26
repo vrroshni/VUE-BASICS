b83be30258356b9db4c3f213b2b2c8b007474a3a
Roshni V R <vrroshni2001@gmail.com>
2023-09-26 12:46:23 +0530

    completed counter app (went through just basics)

--- counter/src/App.vue
+++ counter/src/App.vue
@@ -1,30 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+  import {ref} from 'vue';
+  const count=ref(0)  
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main> 
+    <div>
+      <h2 >The current count is....</h2>
+      <h4>{{ count }}</h4>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+div {
+  text-align: center;
 }
 </style>
