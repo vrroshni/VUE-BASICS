5ec470dcea8806292a5350993ffb11097b77c81c
Roshni V R <vrroshni2001@gmail.com>
2023-09-26 14:09:48 +0530

    completed notes app

---
+++ notes/src/App.vue
@@ -0,0 +1,184 @@
+<script setup>
+import { ref } from 'vue';
+
+const showModal = ref(false)
+const newNote = ref("")
+const notes = ref([])
+const error = ref('')
+
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+const addNote = () => {
+  if (newNote.value.length < 10) {
+    return error.value = "Minimum of 10 characters needed"
+  }
+
+  notes.value.push({
+    id: Math.floor(Math.random() * 10000),
+    text: newNote.value,
+    date: new Date(),
+    backgroundColor: getRandomColor()
+  })
+  showModal.value = false
+  newNote.value = ""
+  error.value = ""
+}
+
+
+
+
+</script>
+
+
+
+<template>
+  <main>
+    <div v-if="showModal" class="overlay">
+      <div class="modal">
+        <textarea v-model.trim="newNote" name="" id="note" cols="30" rows="10">
+      </textarea>
+        <p v-if="error">{{ error }}</p>
+        <button @click="addNote">Add Note</button>
+        <button class="close" @click="showModal = false">Close</button>
+      </div>
+    </div>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button @click="showModal = true">+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card" v-for="note in notes" :style="{ backgroundColor: note.backgroundColor }" :key="note.id">
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 75px;
+}
+
+header button {
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: rgb(21, 20, 20);
+  border-radius: 1000px;
+  color: white;
+  font-size: 20px;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  background-color: rgb(237, 182, 44);
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+
+.date {
+  font-size: 12.5px;
+  margin-top: auto;
+}
+
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  transform: translate(-50%, -50%);
+  top: 50%;
+  left: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 10;
+}
+
+.modal {
+  width: 750px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal button {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: blueviolet;
+  border: none;
+  color: white;
+  cursor: pointer;
+  margin-top: 15px;
+
+}
+
+.modal .close {
+  background-color: red;
+  margin-top: 7px;
+
+}
+.modal p {
+  color: red;
+  font-size: 10px;
+  width: 100%;
+  
+
+}
+
+.modal p {
+  margin-left: auto;
+  font-size: 20px;
+  z-index: 100000;
+  cursor: pointer;
+}
+
+textarea {
+  width: 100%;
+  height: 200px;
+  padding: 5px;
+  font-size: 20px;
+}
+</style>
