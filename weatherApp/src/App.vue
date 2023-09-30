dd45fc30d38d279454c47ffc3e9b9d0383d2c5d6
Roshni V R <vrroshni2001@gmail.com>
2023-09-30 13:44:39 +0530

    completed navbar

--- weatherApp/src/App.vue
+++ weatherApp/src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import SiteNavigation from './components/SiteNavigation.vue';
+
 </script>
 
 <template>
- <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
+    <router-view></router-view>
+
+  </div>
 </template>
